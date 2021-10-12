fddcfd051e015ecd783f12154b3634f210a1d576
ridolenai <92106359+ridolenai@users.noreply.github.com>
2021-10-12 14:48:12 -0500

    Initial Commit
    Product class adds items to cart.

---
+++ product.py
@@ -0,0 +1,20 @@
+
+class Product:
+    def __init__(self, name, price, category):
+        self.name = name
+        self.price = price
+        self.category = category
+
+    def items_to_buy(self):
+        name = []
+        price = []
+        category = []
+        interrogative = input ('Would you like to add something to your cart? 1 for yes and 2 for no')
+        while interrogative != ('2'):
+            new_item = input ('Type the name of the item you would like to add to your cart: ')
+            name.append (new_item)
+            new_price = input ('Type the price of the item you are adding to your cart: ') 
+            price.append(new_price)
+            new_category = input ('Type the category of the item you added to your cart: ')
+            category.append(new_category)
+            interrogative = input ('Would you like to add something to your cart? 1 for yes and 2 for no ')
---
+++ shopping_cart_execution.py
@@ -0,0 +1,14 @@
+
+from product import Product
+
+cart_one = Product ('', '', '')
+
+overpriced_mart_entrance = input ('Would you like to go shopping? 1 for yes 2 for no ')
+
+if overpriced_mart_entrance != ('2'):
+
+    cart_one.items_to_buy 
+
+
+print ('Have a good day, and thank you for shopping at Overpriced Mart.')
+
